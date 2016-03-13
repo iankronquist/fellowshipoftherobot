e6909e56f4eb58069c8a839cc08988eb7d86ec7a
reedslobodin <reedslobodin@gmail.com>
2016-03-12 16:14:43 -0800

    Revert "adsfklja;sdlfkj"
    This reverts commit e4f1f72e1789fd632422ff605b7a23367432fe90.

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FellowshipRedAuto.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FellowshipRedAuto.java
@@ -20,26 +20,19 @@ public class FellowshipRedAuto extends LinearOpMode {
     Servo ClimberServo;
     Servo HopperDoor;
     Servo LiftServo;
+    Servo RightTail;
+    Servo LeftTail;
     Servo RightHangServo;
     Servo LeftHangServo;
-    Servo LeftCowcatcher;
-    Servo RightCowcatcher;
-    Servo beaconServo;
     AnalogInput rollerPhotogate;
     AnalogInput elevatorPhotogate;
     ColorSensor floorSeeker;
-    ColorSensor beaconSeeker;
     GyroSensor gyro;
     final double turnPower = 0;
     final double arcOffset = .9;
     final double movePower = .35;
     final double rollerPower = -.9;
     final double zero = .53;
-    final double cowcatcherOffset = .03;
-    final double higherPower = .6;
-    final double lowerPower = .4;
-    final double rightHit = .3;
-    final double leftHit = .7;
 
 
     public void stopMotors() throws InterruptedException{
@@ -47,47 +40,6 @@ public class FellowshipRedAuto extends LinearOpMode {
         waitOneFullHardwareCycle();
         motorRight.setPower(0);
         waitOneFullHardwareCycle();
-    }
-public void followLine() throws InterruptedException{
-    resetStartTime();
-    while (getRuntime() < 5) {
-        if (floorSeeker.green()>=10) {
-            motorLeft.setPower(higherPower);
-            motorRight.setPower(lowerPower);
-            waitOneFullHardwareCycle();
-        }
-
-        if (floorSeeker.green()<10) {
-            motorLeft.setPower(lowerPower);
-            motorRight.setPower(higherPower);
-            waitOneFullHardwareCycle();
-        }
-        waitOneFullHardwareCycle();
-    }
-
-}
-public void dumpClimbers() throws InterruptedException{
-    ClimberServo.setPosition(1);
-    Thread.sleep(500);
-    ClimberServo.setPosition(.8);
-    Thread.sleep(500);
-    ClimberServo.setPosition(1);
-}
-
-    public void hitBeacon() throws InterruptedException {
-        if (beaconSeeker.red() > 8) {
-            beaconServo.setPosition(rightHit);
-        } else if (beaconSeeker.blue() > 8) {
-            beaconServo.setPosition(leftHit);
-        } else {
-            waitOneFullHardwareCycle();
-        }
-
-    }
-        public void BackAway() throws InterruptedException{
-        motorLeft.setPower(-.8);
-            motorRight.setPower(-.5);
-            Thread.sleep(1000);
     }
 
 
@@ -100,26 +52,27 @@ public void dumpClimbers() throws InterruptedException{
     }
     public void findWhiteStripe() throws InterruptedException {
         while (floorSeeker.green() < 10) {
-            motorLeft.setPower(.5);
-            motorRight.setPower(.5);
+            motorLeft.setPower(.8);
+            motorRight.setPower(.8);
             waitOneFullHardwareCycle();
         }
     }
     public void gyroTurn(int desiredAngle, String direction) throws InterruptedException{
 
         if (direction == "left") {
-            while (Math.abs(desiredAngle-gyro.getHeading())>5) {
-                motorLeft.setPower(.8);
-                motorRight.setPower(-.8);
+            while (gyro.getHeading() > desiredAngle) {
+                double proportionalSpeed = .7+.3*((desiredAngle-gyro.getHeading())/desiredAngle);
+                motorLeft.setPower(proportionalSpeed);
+                motorRight.setPower(-proportionalSpeed);
                 telemetry.addData("gyro", gyro.getHeading());
                 waitOneFullHardwareCycle();
             }
         }
         if (direction == "right") {
-            while (Math.abs(desiredAngle-gyro.getHeading())>5) {
-
-                motorLeft.setPower(-.8);
-                motorRight.setPower(.8);
+            while (gyro.getHeading() < desiredAngle) {
+                double proportionalSpeed = .7+.3*((desiredAngle-gyro.getHeading())/desiredAngle);
+                motorLeft.setPower(-proportionalSpeed);
+                motorRight.setPower(proportionalSpeed);
                 telemetry.addData("gyro", gyro.getHeading());
                 waitOneFullHardwareCycle();
             }
@@ -130,47 +83,20 @@ public void dumpClimbers() throws InterruptedException{
 
     @Override
     public void runOpMode() throws InterruptedException {
-Thread.sleep(2000);
+
         map();
         waitForStart();
-        RollerMotor.setPower(rollerPower);
-        motorLeft.setPower(.8);
-        motorRight.setPower(.8);
-        Thread.sleep(500);
-        waitOneFullHardwareCycle();
-        stopMotors();
-        waitOneFullHardwareCycle();
-        gyroTurn(315, "left");
         waitForNextHardwareCycle();
         findRedStripe();
         waitOneFullHardwareCycle();
         stopMotors();
         waitOneFullHardwareCycle();
         telemetry.addData("angle", gyro.getHeading());
-        gyroTurn(0, "right");
+        gyroTurn(45,"right");
         waitOneFullHardwareCycle();
         stopMotors();
         waitOneFullHardwareCycle();
         findWhiteStripe();
-        waitOneFullHardwareCycle();
-        stopMotors();
-        waitOneFullHardwareCycle();
-        motorLeft.setPower(-.8);
-        motorRight.setPower(-.8);
-        Thread.sleep(250);
-        waitOneFullHardwareCycle();
-        stopMotors();
-        gyroTurn(270, "left");
-        waitOneFullHardwareCycle();
-        followLine();
-        waitOneFullHardwareCycle();
-        Thread.sleep(500);
-        dumpClimbers();
-        waitOneFullHardwareCycle();
-        hitBeacon();
-        waitOneFullHardwareCycle();
-        Thread.sleep(500);
-        BackAway();
         waitOneFullHardwareCycle();
         stopMotors();
 
@@ -188,9 +114,6 @@ Thread.sleep(2000);
         LeftZipline = hardwareMap.servo.get("LeftZipline");
         RightZipline = hardwareMap.servo.get("RightZipline");
         RightZipline.setDirection(Servo.Direction.REVERSE);
-        LeftCowcatcher = hardwareMap.servo.get("LeftCowcatcher");
-        RightCowcatcher = hardwareMap.servo.get("RightCowcatcher");
-        LeftCowcatcher.setDirection(Servo.Direction.REVERSE);
         waitOneFullHardwareCycle();
         motorLeft.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
         motorRight.setMode(DcMotorController.RunMode.RUN_WITHOUT_ENCODERS);
@@ -204,27 +127,29 @@ Thread.sleep(2000);
         ClimberServo = hardwareMap.servo.get("ClimberServo");
         HopperDoor = hardwareMap.servo.get("HopperDoor");
         HopperDoor.setPosition(.5);
-        beaconServo = hardwareMap.servo.get("beaconServo");
-        beaconServo.setPosition(.5);
         floorSeeker = hardwareMap.colorSensor.get("floorSeeker");
-        beaconSeeker = hardwareMap.colorSensor.get("beaconSeeker");
         gyro = hardwareMap.gyroSensor.get("gyro");
         gyro.calibrate();
+
         //ClimberServo.setPosition(0.5);
         RightHangServo = hardwareMap.servo.get("RightHangServo");
         LeftHangServo  = hardwareMap.servo.get("LeftHangServo");
         LeftHangServo.setDirection(Servo.Direction.REVERSE);
         rollerPhotogate = hardwareMap.analogInput.get("rollerPhotogate");
         elevatorPhotogate = hardwareMap.analogInput.get("elevatorPhotogate");
+        RightTail=hardwareMap.servo.get("RightTail");
+        LeftTail =hardwareMap.servo.get("LeftTail");
+        LeftTail.setDirection(Servo.Direction.REVERSE);
         waitOneFullHardwareCycle();
         ClimberServo.setPosition(0);
         LeftZipline.setPosition(.5);
         RightZipline.setPosition(.5);
         waitOneFullHardwareCycle();
+        RightTail.setPosition(0);
+        LeftTail.setPosition(0);
+        waitOneFullHardwareCycle();
         RightHangServo.setPosition(zero+.01);
         LeftHangServo.setPosition(zero+.035);
-        RightCowcatcher.setPosition(.7+cowcatcherOffset);
-        LeftCowcatcher.setPosition(.7);
         waitOneFullHardwareCycle();
     }
 
